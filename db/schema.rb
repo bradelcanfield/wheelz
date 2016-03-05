3d9d8aacdce4407eab9287fd1a02f11a11997e25
Alex Melcon <amelc001@fiu.edu>
2016-03-05 16:53:10 -0500

    added admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303191204) do
+ActiveRecord::Schema.define(version: 20160305214830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,13 @@ ActiveRecord::Schema.define(version: 20160303191204) do
     t.string   "phone"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.boolean  "admin",               default: false
   end
 
   create_table "routes", force: :cascade do |t|
