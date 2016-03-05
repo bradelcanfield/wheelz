8a7cb79eb8de3587c0bf6338a778b3bbdd960b3d
Alex Melcon <amelc001@fiu.edu>
2016-03-05 18:22:27 -0500

    changed signup

--- app/controllers/commuters_controller.rb
+++ app/controllers/commuters_controller.rb
@@ -1,8 +1,9 @@
 class CommutersController < ApplicationController
   before_action :authorize, only: [:profile, :edit, :index]
+  before_action :admin_user,     only: :destroy
 
   def index
-    @commuters = Commuter.all
+    @commuters = Commuter.paginate(page: params[:page])
   end
 
   def show
@@ -17,24 +18,32 @@ class CommutersController < ApplicationController
   end
 
   def create
-    commuter = Commuter.new(commuter_params)
-    if commuter.save
-      session[:commuter_id] = commuter.id
-      redirect_to '/profile'
+    @commuter = Commuter.new(commuter_params)
+    if @commuter.save
+      flash[:success] = "Welcome to Wheelz!"
+      redirect_to @commuter
     else
-      flash[:danger] = 'Name, email, and password fields must be filled.'
-      redirect_to '/profile/new'
+      render 'new'
     end
   end
 
   def edit
+    @commuter = Commuter.find(params[:id])
   end
 
   def destroy
+    Commuter.find(params[:id]).destroy
+    flash[:success] = "Commuter deleted"
+    redirect_to commuters_url
   end
 
   private
   def commuter_params
     params.require(:commuter).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
